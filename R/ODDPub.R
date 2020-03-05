691d0c571a29317e07491fc9d80feb63737567e2
Nico Riedel <nico.riedel@bihealth.de>
2020-03-05 16:05:47 +0100

    Added example text files for the examples in the documentation

--- R/ODDPub.R
+++ R/ODDPub.R
@@ -10,7 +10,7 @@
 #'
 #' @return Logical vector describing the conversion success for each PDF file.
 #'
-#' @examples pdf_convert("./my_PDF_folder/", "./my_txt_folder/")
+#' @examples pdf_convert("./example_PDFs/", "./examples/")
 pdf_convert <- function(PDF_folder, output_folder, clusters = 10)
 {
   #parallelize pdf conversion
@@ -45,7 +45,7 @@ pdf_convert <- function(PDF_folder, output_folder, clusters = 10)
 #' @return List with one element per document.
 #' Each document is split into its sentences and saved as a vector of strings.
 #'
-#' @examples pdf_load("./my_txt_folder/")
+#' @examples pdf_load("./examples/")
 pdf_load <- function(pdf_text_folder)
 {
 
@@ -82,7 +82,7 @@ pdf_load <- function(pdf_text_folder)
 #' @return Tibble with one row per screened document and the filename and logical values for open data
 #' and open code detection as columns.
 #'
-#' @examples open_data_search(pdf_load("./my_txt_folder/"))
+#' @examples open_data_search(pdf_load("./examples/"))
 open_data_search <- function(PDF_text_sentences)
 {
   #one part of the keyword search acts on the tokenized sentences while another part acts on the full text
@@ -115,7 +115,7 @@ open_data_search <- function(PDF_text_sentences)
 #' and open code detection as columns plus an additional column containing the detected sentences
 #' for each of the checked keyword category.
 #'
-#' @examples open_data_sentences(pdf_load("./my_txt_folder/"))
+#' @examples open_data_sentences(pdf_load("./examples/"))
 open_data_sentences <- function(PDF_text_sentences)
 {
   #search for open data keywords in the full texts
